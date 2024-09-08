5508b5c3f46e86ec50c9f94738df3ddbfb78b732
Charu <73329946+charu-kumar@users.noreply.github.com>
2024-09-08 10:20:23 +0530

    Update AWS_IAC_Terraform.tf

--- Terraform/AWS IAC/AWS_IAC_Terraform.tf
+++ Terraform/AWS IAC/AWS_IAC_Terraform.tf
@@ -125,7 +125,7 @@ resource "aws_instance" "ec2-public" {
                 #!/bin/bash
                 sudo apt update -y
                 sudo apt install apache2 -y
-                sudo systemctl enable apache2 && systemctl start apache2
+                sudo systemctl enable apache2 && sudo systemctl start apache2
                 sudo bash -c 'echo First web server > /var/www/http/index.html'
                 EOF
   
